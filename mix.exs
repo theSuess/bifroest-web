a6ea57321692b04b38b477271ebe285bd72b71b9
Dominik Süß <dominik.suess@outlook.at>
2017-05-24 13:06:50 +0200

    removed useless meta

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bifroest.Mixfile do
 
   def project do
     [app: :bifroest,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
